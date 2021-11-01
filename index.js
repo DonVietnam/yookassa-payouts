a55a2e03c880011c8dac33b327b41603ded673b3
DanilSord <kincharov99@gmail.com>
2021-11-01 19:43:25 +0700

    Testing ssl

---
+++ index.js
@@ -0,0 +1,45 @@
+const path = require( 'path' );
+const crypto = require( 'crypto' );
+const https = require('https');
+const parser = require('fast-xml-parser');
+const fs = require( 'fs' );
+
+const certPath = path.resolve( 'D:/certs/mine', 'cert.pem' );
+const keyPath = path.resolve( 'D:/certs/mine', 'key.pem' );
+
+const cert = fs.readFileSync( certPath );
+const key = fs.readFileSync( keyPath );
+
+const xml = '<balanceRequest agentId="123" clientOrderId="12345" requestDT="2011-07-01T20:38:00.000Z"/>';
+const url = 'https://payouts.yookassa.ru:9094/webservice/deposition/api/balance';
+
+const encodedData = crypto.publicEncrypt( publicKey, Buffer.from( xml ) );
+
+const options = {
+  hostname: 'payouts.yookassa.ru',
+  port: 9094,
+  path: '/webservice/deposition/api/balance',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/pkcs7-mime',
+  },
+  cert,
+  key
+};
+
+const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', d => {
+    process.stdout.write(d)
+  })
+})
+
+req.on('error', error => {
+  console.error(error)
+})
+
+req.write(encodedData)
+
+//req.end()
+//console.log( encodedData );
